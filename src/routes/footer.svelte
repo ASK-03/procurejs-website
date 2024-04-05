955cab1bb7cacefb722a4ae4f905542317f175a3
ASK-03 <abhisheksinghk@iitbhilai.ac.in>
2024-04-06 03:46:32 +0530

    removed inView from footer

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,19 +1,8 @@
 <script>
 	import Footer from '@/components/Footer.svelte';
-	import { inview } from 'svelte-inview';
-
-	let isInView;
-	const options = {
-		rootMargin: '100px',
-		unobserveOnEnter: true
-	};
 
 	const handleChange = ({ detail }) => (isInView = detail.inView);
 </script>
 
-<div use:inview={options} on:inview_change={handleChange}>
-	{#if isInView}
-		<Footer />
-	{/if}
-</div>
+<Footer />
 <!-- Make it like the figma design -->
